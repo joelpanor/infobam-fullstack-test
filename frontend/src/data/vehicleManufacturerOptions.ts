8063f1f46afde4c526bdcd82a607a471e51feaf8
joelpanor <joel.panor@gmail.com>
2025-06-03 09:49:13 -0400

    feat: create filter Manufacturer data mock data

---
+++ frontend/src/data/vehicleManufacturerOptions.ts
@@ -0,0 +1,12 @@
+export const vehicleManufacturerOptions = [
+  "BMW",
+  "Tesla",
+  "Mercedes-Benz",
+  "Porsche",
+  "Toyota",
+  "Audi",
+  "Ford",
+  "Lexus",
+  "Volkswagen",
+  "Chevrolet"
+];
