51d83e2629293ff91862af2b002d4814bd3eca02
joelpanor <joel.panor@gmail.com>
2025-06-03 09:49:42 -0400

    feat: create filter vehicleType option mock data

---
+++ frontend/src/data/vehicleTypeOptions.ts
@@ -0,0 +1,10 @@
+import { VehicleType } from "@/types/Vehicle";
+
+export const vehicleTypeOptions = [
+    VehicleType.SUV,
+    VehicleType.SEDAN,
+    VehicleType.TRUCK,
+    VehicleType.SPORTS,
+    VehicleType.LUXURY,
+    VehicleType.ELECTRIC,
+];
