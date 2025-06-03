8d2f53a7e444500349f5d8e9bd5256750c3c5faa
joelpanor <joel.panor@gmail.com>
2025-06-03 09:16:56 -0400

    feat: create api route for retrieve vehicle data

---
+++ frontend/src/app/api/vehicles/route.ts
@@ -0,0 +1,29 @@
+import { vehicleList } from "@/data/vehicleMockData";
+import { vehicleService } from "@/services/vehicleService";
+import { VehicleResponse } from "@/types/Vehicle";
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+
+const VehiclePaginationSchema = z.object({
+  page: z.number().min(1),
+});
+
+export async function POST(request: NextRequest): Promise<NextResponse<VehicleResponse>> {
+
+  //Validate the request body
+  const { page } = VehiclePaginationSchema.parse(await request.json());
+  
+  const pageSize = 3;
+  const vehicles = await vehicleService.getPaginatedVehicles(vehicleList, page, pageSize);
+  const totalVehicles = vehicleList.length;
+ 
+  const totalPages = Math.ceil(totalVehicles / pageSize);
+
+  const response = {
+    data: vehicles,
+    totalPages,
+  };
+
+  return NextResponse.json(response);
+
+}
