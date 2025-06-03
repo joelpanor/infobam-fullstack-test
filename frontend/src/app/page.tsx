403a1f1f8156d54573807d0ad01b5e30a42e3654
joelpanor <joel.panor@gmail.com>
2025-06-03 11:12:36 -0400

    fix: add full height to container

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import VehiculeList from "@/components/vehicle/VehicleList";
 export default async function Home() {
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto min-h-screen">
       <VehiculeList/>
     </div>
   );
