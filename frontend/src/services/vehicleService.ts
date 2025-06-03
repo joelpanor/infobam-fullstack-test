c1cdbfc710213e81944a8969f35e4d787f112de9
joelpanor <joel.panor@gmail.com>
2025-06-03 09:51:15 -0400

    feat: create getFilteredVehicles function to vehicleService

--- frontend/src/services/vehicleService.ts
+++ frontend/src/services/vehicleService.ts
@@ -1,11 +1,42 @@
 import { Vehicle, VehicleFilterData } from "@/types/Vehicle";
 import { vehicleList } from "@/data/vehicleMockData";
+import { vehicleManufacturerOptions } from "@/data/vehicleManufacturerOptions";
+import { vehicleTypeOptions } from "@/data/vehicleTypeOptions";
+import { vehicleYearOptions } from "@/data/vehicleYearOptions";
 
 
 class VehicleService {
 
   getPaginatedVehicles(vehicles : Vehicle[], page: number, pageSize: number) : Vehicle[] {
     return vehicles.slice((page - 1) * pageSize, page * pageSize);
+  }
+
+  getFilteredVehicles(vehicles : Vehicle[],filterData: VehicleFilterData) : Vehicle[] {
+    return vehicles.filter((vehicle) => {
+      if(filterData.manufacturer.length > 0 && !filterData.manufacturer.includes(vehicle.manufacturer)) {
+        return false;
+      }
+      if(filterData.type.length > 0 && !filterData.type.includes(vehicle.type)) {
+        return false;
+      }
+      if(filterData.year.length > 0 && !filterData.year.includes(vehicle.year.toString())) {
+        return false;
+      }
+      return true;
+    });
+  }
+
+
+  async getVehicleManufacturers() {
+    return vehicleManufacturerOptions;
+  }
+
+  async getVehicleTypes() {
+    return vehicleTypeOptions;
+  }
+
+  async getVehicleYears() {
+    return vehicleYearOptions;
   }
 
   getTotalVehicles() : number {
